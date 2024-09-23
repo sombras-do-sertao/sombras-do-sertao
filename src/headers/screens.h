b2fdf2574578e9028c65f2126c69a823e4885e2a
pedrofnseca <pedropereira.pp264@gmail.com>
2024-09-22 21:14:43 -0300

    fix: destroy allegro screens
    Co-Authored-By: André Cerqueira Costa <160081017+AndreCcerq@users.noreply.github.com>

--- src/headers/screens.h
+++ src/headers/screens.h
@@ -8,13 +8,16 @@
 
 extern ALLEGRO_BITMAP *bg_home;
 void setupHome(struct AllegroGame *game);
+void destroyHome(void);
 bool drawHome(struct AllegroGame *game, GameState *gameState);
 
 bool drawConfig(struct AllegroGame *game);
+void destroyConfig(void);
 void setupButtonsConfig(struct AllegroGame *game);
 
 extern ALLEGRO_BITMAP *bg_game;
 void setupGame();
+void destroyGame();
 bool drawGame(struct AllegroGame *game);
 
 #endif
