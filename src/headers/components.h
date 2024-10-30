3c6446c69b874e3498925a9d8ac04c182a403fa7
Eduardo Leal Aruth <eduardolealaruth@gmail.com>
2024-10-30 17:01:15 -0300

    feat: setup saves

--- src/headers/components.h
+++ src/headers/components.h
@@ -23,11 +23,27 @@ struct Button {
   enum ButtonType type;
 };
 
+struct saveFile {
+  int x;
+  int y;
+  int width;
+  int height; 
+  char *name;
+  char *alias; 
+  ALLEGRO_FONT *font;
+  ALLEGRO_COLOR font_color;
+  ALLEGRO_BITMAP *image;
+};
+
 #define BUTTONS_CONFIG_COUNT 2
 extern struct Button BUTTONS_CONFIG[BUTTONS_CONFIG_COUNT];
 
 #define BUTTONS_HOME_COUNT 4
 extern struct Button BUTTONS_HOME[BUTTONS_HOME_COUNT];
+
+#define SAVEFILES_COUNT 3
+extern struct saveFile FILES[SAVEFILES_COUNT];
+
 
 bool drawButton(struct Button *button);
 
--- src/screens/saves.c
+++ src/screens/saves.c
@@ -3,3 +3,60 @@
 #include "../headers/helper.h"
 #include <stdio.h>
 
+struct saveFile FILES[SAVEFILES_COUNT];
+
+ALLEGRO_FONT *fontSettings;
+ALLEGRO_BITMAP *fileBackground
+setupSaves(){
+  fontSettings = al_load_font("assets/fonts/LilitaOne-Regular.ttf",32,0);
+  fileBackground = al_load_bitmap("assets/images/background/save.jpg");
+  int width = 404;
+  int height = 72;
+
+  struct saveFile save1 = {
+    WIDTH_SCREEN /2 - width/2,
+    HEIGHT_SCREEN / 2 - 72,
+    width,
+    height,
+    "Pedro",
+    "Save 01", 
+    fontSettings,
+    AL_COLOR_WHITE,
+    fileBackground,
+  }; save1 = FILES[0];
+
+  struct saveFile save2 = {
+    WIDTH_SCREEN /2 - width/2,
+    HEIGHT_SCREEN / 2 + 72,
+    width,
+    height,
+    "Lucas",
+    "Save 02", 
+    fontSettings,
+    AL_COLOR_WHITE,
+    fileBackground,
+  }; save2 = FILES[1];
+
+  struct saveFile save3 = {
+    WIDTH_SCREEN /2 - width/2,
+    HEIGHT_SCREEN / 2 + 216,
+    width,
+    height,
+    "Eduardo",
+    "Save 03", 
+    fontSettings,
+    AL_COLOR_WHITE,
+    fileBackground,
+  }; save3 = FILES[2];
+
+}
+
+ /*int x;
+  int y;
+  int width;
+  int height; 
+  char *name;
+  char *alias; 
+  ALLEGRO_FONT *font;
+  ALLEGRO_COLOR font_color;
+  ALLEGRO_BITMAP *image;*/
